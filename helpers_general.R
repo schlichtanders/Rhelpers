a1e8dba3ebb741db11dbf3e036c3dc37820c19b3
Stephan Sahm <Stephan.Sahm@gmx.de>
2015-11-14 10:04:28 +0100

    added flatten option

--- helpers_general.R
+++ helpers_general.R
@@ -13,3 +13,39 @@ attach.all <- function (x, overwrite = NA, name = "attach.all")  {
     if (overwrite) remove(list = rem, envir = .GlobalEnv)
     attach(x, name = name)
 }
+
+
+
+meshgrid = function(...){
+    args = argsenv(...)
+    # first extract additional parameters:
+    i_flatten = match("flatten", attr(args, 'formalnames'))
+    if(is.na(i_flatten)){
+        flatten = FALSE
+        tagnames = attr(args, 'tagnames')
+        formalnames = attr(args, 'formalnames')
+    } else {
+        flatten = args[[ attr(args, 'tagnames')[i_flatten] ]]
+        tagnames = attr(args, 'tagnames')[-i_flatten]
+        formalnames = attr(args, 'formalnames')[-i_flatten]
+    }
+
+    # then proceed
+    N = length(tagnames)
+    mylength = function(t) length(args[[t]])
+    nargs = mapply(mylength, tagnames)
+    single_meshgrid = function(i, t){
+        Nrep = prod(nargs[names(nargs)!=t])
+        rot = rotation(N, i-1)
+        X = aperm(array(rep(args[[t]], Nrep), nargs[rot]), rot)
+        if (flatten)
+            dim(X) <- NULL
+        return(X)
+    }
+    l = Map(single_meshgrid, 1:N, tagnames)
+
+    if(all( formalnames != ""))
+        names(l)<-formalnames
+    return(l)
+
+}
